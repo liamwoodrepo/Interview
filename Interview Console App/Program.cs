e0da47b95a427eec49ee87a29715f254adcd93c5
Liam Wood <liam.woodz14@hotmail.co.uk>
2020-10-06 11:13:52 +0100

    Console app update

--- Interview Console App/Program.cs
+++ Interview Console App/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Interview_Web_API.Database.Entity;
+using Interview_Web_API.Helpers;
+using Interview_Web_API.Models;
+using System;
+using System.IO;
 
 namespace Interview_Console_App
 {
@@ -6,7 +10,44 @@ namespace Interview_Console_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please specify the file path for the data.txt file: ");
+
+            while(true)
+            {
+                var userInput = Console.ReadLine();
+
+                var filename = Path.GetFileName(userInput);
+
+                if (filename.ToUpper().Equals("DATA.TXT"))
+                {
+                    try
+                    {
+                        var employees = ConvertCSVToEmployee.ConvertCSVFileTOEmployees(userInput);
+
+                        using (var db = new ConnectionString())
+                        {
+                            foreach (Employee employee in employees)
+                            {
+                                db.Add(employee);
+
+                            }
+                            db.SaveChanges();
+                        }
+
+                        Console.WriteLine("Employees have been added successfully!");
+                        Console.WriteLine("Please specify the file path for the next data.txt file: ");
+
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Unable to to process file, please try again!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to to locate file, please try again!");
+                }
+            }
         }
     }
 }
--- Interview Web API/Models/ConnectionString.cs
+++ Interview Web API/Models/ConnectionString.cs
@@ -1,5 +1,6 @@
 ﻿using Interview_Web_API.Database.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,17 @@ namespace Interview_Web_API.Models
         public ConnectionString(DbContextOptions<ConnectionString> options):base(options)
         {
 
+        }
+
+        public ConnectionString()
+        {
+        }
+
+        private const string connectionString = "Data Source=DESKTOP-4P15GJ5\\MSSQLSERVER01;Initial Catalog=Interview;Integrated Security=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
         }
 
         public DbSet<Employee> Employee { get; set; }
